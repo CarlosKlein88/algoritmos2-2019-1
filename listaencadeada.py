0270acaada797b95c32ae241a8676da792307a43
CarlosKlein88 <42423291+CarlosKlein88@users.noreply.github.com>
2019-06-05 21:18:56 -0300

    Add files via upload
    Remoção parcialmente adicionada

--- listaencadeada.py
+++ listaencadeada.py
@@ -37,14 +37,37 @@ class ListaEncadeada:
         if self.head is None:
             return None
         iter = self.head
-        while iter.next is not None:
+        while iter is not None:
             print (iter.value)
             iter = iter.next
 
-prepend(0)
-append(1)
-append(2)
-append(3)
-prepend(4)
-prepend(5)
-printLista()
+    def remove_if(self, value):
+        v = value
+        iter = self.head
+        if iter is None:
+            return 
+        elif iter.next is None:
+            self.head = None
+            self.tail = None
+        elif iter == v:
+            self.head = iter.next
+        else:
+            while iter is not v:
+                ant = iter
+                iter = iter.next
+                if iter == v and iter.next is not None:
+                    ant = iter.next
+                if iter == v and iter.next is None:
+                    self.tail = ant
+
+
+lista = ListaEncadeada()
+lista.remove_if(4)
+lista.prepend(0)
+lista.append(1)
+lista.append(2)
+lista.append(3)
+lista.prepend(4)
+lista.prepend(5)
+lista.remove_if(4)
+lista.printLista()
